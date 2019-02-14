f80520121596f7bec9f20405dae55d2a8539a1d8
Code Nation 13 <codenation@CN-13.lan>
2019-02-14 16:55:17 +0000

    Updated dice challenge

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+var scoreTotal = 0;
 
+function rollDice() {
+    var theDice = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("showdice").innerHTML = theDice;
+    scoreTotal = +scoreTotal + +theDice;
+    document.getElementById("scoretotal").innerHTML = scoreTotal;
+
+    if (theDice == 1) {
+        scoreTotal = 0;
+        document.getElementById("diceimage").src = "./img/dice1.png";
+    }
+
+    if (theDice == 2) {
+        document.getElementById("diceimage").src = "./img/dice2.png";
+    }
+
+    if (theDice == 3) {
+        document.getElementById("diceimage").src = "./img/dice3.png";
+    }
+
+    if (theDice == 4) {
+        document.getElementById("diceimage").src = "./img/dice4.png";
+    }
+
+    if (theDice == 5) {
+        document.getElementById("diceimage").src = "./img/dice5.png";
+    }
+
+    if (theDice == 6) {
+        document.getElementById("diceimage").src = "./img/dice6.png";
+
+    } else if (scoreTotal >= 21) {
+        document.getElementById("winner").innerHTML = "Congratulations, you have won!!!";
+        scoreTotal = 0;
+
+    } else if (scoreTotal == 0) {
+        document.getElementById("loser").innerHTML = "You have lost, try again"
+
+    } else {
+        document.getElementById("winner").innerHTML = "";
+        document.getElementById("loser").innerHTML = "";
+    }
+}
