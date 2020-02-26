2a5507769f2e26953d10509dd81cc3271102221b
BenJS1993 <32497552+BenJS1993@users.noreply.github.com>
2020-02-26 00:18:11 +0000

    update

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 var scoreTotal = 0;
 
 function rollDice() {
-    var theDice = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("playerDie").innerHTML = theDice;
-    scoreTotal = +scoreTotal + +theDice;
+    var theDie = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("playerDie").innerHTML = theDie;
+    scoreTotal = +scoreTotal + +theDie;
     document.getElementById("scoretotal").innerHTML = scoreTotal;
 
-    if (theDice == 1) {
+    if (theDie == 1) {
         scoreTotal = 0;
         document.getElementById("playerDie").src = "./img/dice1.png";
     }
 
-    if (theDice == 2) {
+    if (theDie == 2) {
         document.getElementById("playerDie").src = "./img/dice2.png";
     }
 
-    if (theDice == 3) {
+    if (theDie == 3) {
         document.getElementById("playerDie").src = "./img/dice3.png";
     }
 
-    if (theDice == 4) {
+    if (theDie == 4) {
         document.getElementById("playerDie").src = "./img/dice4.png";
     }
 
-    if (theDice == 5) {
+    if (theDie == 5) {
         document.getElementById("playerDie").src = "./img/dice5.png";
     }
 
-    if (theDice == 6) {
+    if (theDie == 6) {
         document.getElementById("playerDie").src = "./img/dice6.png";
 
     } else if (scoreTotal >= 21) {
-        document.getElementById("winner").innerHTML = "Congratulations, you have won!!!";
+        document.getElementById("winner").innerHTML = "You won!!!";
         scoreTotal = 0;
 
     } else if (scoreTotal == 0) {
-        document.getElementById("loser").innerHTML = "You have lost, try again"
+        document.getElementById("loser").innerHTML = "You lost, try again"
 
     } else {
         document.getElementById("winner").innerHTML = "";
