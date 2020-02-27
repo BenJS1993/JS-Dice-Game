be451ebd275769bbf45ab083574dd81aca0705a2
BenJS1993 <32497552+BenJS1993@users.noreply.github.com>
2020-02-27 16:03:20 +0000

    update

--- script.js
+++ script.js
@@ -2,38 +2,31 @@ var scoreTotal = 0;
 
 function rollDice() {
     var theDie = Math.floor(Math.random() * 6) + 1;
-    /* document.getElementById("playerDie").innerHTML = theDie; */
     scoreTotal = +scoreTotal + +theDie;
     document.getElementById("scoretotal").innerHTML = scoreTotal;
 
     if (theDie == 1) {
         scoreTotal = 0;
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice1.png";
     }
 
     if (theDie == 2) {
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice2.png";
     }
 
     if (theDie == 3) {
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice3.png";
     }
 
     if (theDie == 4) {
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice4.png";
     }
 
     if (theDie == 5) {
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice5.png";
     }
 
     if (theDie == 6) {
-        //document.getElementById("playerDie")
         playerDie.src = "./img/dice6.png";
 
     } else if (scoreTotal >= 21) {
